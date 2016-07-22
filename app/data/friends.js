14305f0c7ece63c19948e048e2a865414bfbe8c8
Monica Szabo <monicaszb@gmail.com>
2016-07-22 11:46:44 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Monica",
+	"photo" : "",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+	"name" : "Simon",
+	"photo" : "",
+	"scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+	"name" : "Rhea",
+	"photo" : "",
+	"scores" : [3,3,3,3,3,3,3,3,3,3]
+}];
+
+
+module.exports = friends;
